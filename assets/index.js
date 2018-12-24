faf0c9174a03d6d87cee13bb3ad779a8f61cac10
David Paiva <pillgp@icloud.com>
2018-12-24 01:11:44 +0000

    Check if the guild is valid / exists / has widgets enabled

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,40 @@
 $(document).ready(function(){
-    $('#generator').on('submit', (e) => {
+    $('#generator').on('submit', async (e) => {
         e.preventDefault();
-        if($('#serverId').val() == '') alert('You have to insert a valid id!');
-        let serverId = $('#serverId').val();
-        let styleId = $('#styleId').val();
-        const finalUrl = `https://discordapp.com/api/guilds/${serverId}/widget.png?style=banner${styleId}&t=1512006306528`;
-        // Check if got unknow guild
-        Swal({
-            title: 'Gotcha!',
-            text: 'I found this',
-            imageUrl: finalUrl,
-            imageAlt: 'If you are seeing this, it means that it gave some problem, maybe the id is incorrect or the widgets of your server is disabled'
+        const serverId = $('#serverId').val();
+        if(serverId.split('').length !== 18 || parseInt(serverId) === NaN) {
+            return Swal({
+                title: 'Invalid ID',
+                text: `"${serverId}" isn't a valid server ID. You need to give me a valid server ID, such as 297732013006389252 for example.`,
+                type: 'error'
+            });
+        }
+        $.get(`https://discordapp.com/api/guilds/${serverId}/widget.json`)
+            .then((guild) => {
+                const styleId = $('#styleId').val();
+                const imageUrl = `https://discordapp.com/api/guilds/${serverId}/widget.png?style=banner${styleId}&t=1512006306528`;
+                return Swal({
+                    title: 'Gotcha!',
+                    text: `Here's the guild widget for "${guild.name}"!`,
+                    imageUrl
+                });
+            })
+            .catch((e) => {
+                const cases = {
+                    404: {
+                        title: 'Unknown Guild',
+                        text: 'It seems that the ID you submitted isn\'t a valid guild ID. Check again the guild ID you submitted.'
+                    },
+                    403: {
+                        title: 'Widget Disabled',
+                        text: 'The guild you gave me has the widget disabled. Please, talk to an admin so that they can enable the widget in that guild.'
+                    }
+                }
+                return Swal({
+                    title: cases[e.status].title,
+                    text: cases[e.status].text,
+                    type: 'error'
+                });
         });
     });
 });
